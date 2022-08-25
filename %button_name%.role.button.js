c764c4853013776d032222ed2940768071593243
baeonette <baeonette@gmail.com>
2022-08-25 12:36:56 -0700

    Update %button_name%.role.button.js

--- %button_name%.role.button.js
+++ %button_name%.role.button.js
@@ -2,6 +2,47 @@ module.exports = {
     button_id: '%button_name%',
     description: '%button_name% role button',
     async execute(interaction) {
+        await interaction.deferReply({ ephemeral: true });
+
+        const member = interaction.message.guild.members.cache.get(interaction.user.id);
+
+        const role = '%role_id%';
+        
+        member.roles.cache.find((r) => r.id === `${role}`)
+            ? member.roles
+                  .remove(`${role}`)
+                  .then(async () => {
+                      await interaction
+                          .editReply({
+                              content: `Removed the <@&${role}> role!`,
+                              ephemeral: true,
+                          })
+                          .catch((err) => console.log(err));
+                  })
+                  .catch((error) => {
+                      if (error.message === 'Missing Permissions')
+                          return interaction.editReply({
+                              content: `**Unable to remove the <@&${role}> role :/**\n\n>>> Contact a server admin about moving my role above the <@&${role}> role so I can remove it :)`,
+                              ephemeral: true,
+                          });
+                  })
+            : member.roles
+                  .add(`${role}`)
+                  .then(async () => {
+                      await interaction
+                          .editReply({
+                              content: `Added the <@&${role}> role!`,
+                              ephemeral: true,
+                          })
+                          .catch((err) => console.log(err));
+                  })
+                  .catch((error) => {
+                      if (error.message === 'Missing Permissions')
+                          return interaction.editReply({
+                              content: `**Unable to add the <@&${role}> role :/**\n\n>>> Contact a server admin about moving my role above the <@&${role}> role so I can add it :)`,
+                              ephemeral: true,
+                          });
+                  });
         return interaction.reply({ content: 'The %button_name% button was clicked!', ephemeral: false });
     },
 };
