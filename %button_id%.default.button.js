e4b491bf35983b7009b88a396f13d3150f97ea8c
baeonette <baeonette@gmail.com>
2022-08-25 13:48:15 -0700

    Update %button_id%.default.button.js

--- %button_id%.default.button.js
+++ %button_id%.default.button.js
@@ -2,6 +2,6 @@ module.exports = {
     button_id: '%button_id%',
     description: '%button_id% button',
     async execute(interaction) {
-        return interaction.reply({ content: 'The %button_name% button was clicked!', ephemeral: false });
+        return interaction.reply({ content: 'The %button_id% button was clicked!', ephemeral: false });
     },
 };
