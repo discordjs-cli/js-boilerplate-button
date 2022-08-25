b6990b53b7c5c6e1ecde72a9519d97c95aa1d2ef
baeonette <baeonette@gmail.com>
2022-08-25 12:32:26 -0700

    Update %button_name%.role.button.js

--- %button_name%.role.button.js
+++ %button_name%.role.button.js
@@ -1,6 +1,6 @@
 module.exports = {
-    name: '%button_name%',
-    description: '%button_name% button',
+    button_id: '%button_name%',
+    description: '%button_name% role button',
     async execute(interaction) {
         return interaction.reply({ content: 'The %button_name% button was clicked!', ephemeral: false });
     },
